0ff767332e6f6b3c46fbca9d5e4af188824b44a1
samantrs <https://samantrs@git.rabobank.nl>
2022-11-29 23:33:22 +0100

    kafka producer module

--- order-service-infrastructure/order-service-kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/service/KafkaProducer.java
+++ order-service-infrastructure/order-service-kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/service/KafkaProducer.java
@@ -2,10 +2,10 @@ package com.food.ordering.system.kafka.producer.service;
 
 import org.apache.avro.specific.SpecificRecordBase;
 import org.springframework.kafka.support.SendResult;
-import org.springframework.util.concurrent.ListenableFutureCallback;
 
 import java.io.Serializable;
+import java.util.concurrent.CompletableFuture;
 
 public interface KafkaProducer<K extends Serializable, V extends SpecificRecordBase> {
-    void send(String topicName, K key, V message, ListenableFutureCallback<SendResult<K, V>> callback);
+    void send(String topicName, K key, V message, CompletableFuture<SendResult<K, V>> futureCallback);
 }
--- order-service-infrastructure/order-service-kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/service/impl/KafkaProducerImpl.java
+++ order-service-infrastructure/order-service-kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/service/impl/KafkaProducerImpl.java
@@ -1,14 +1,13 @@
 package com.food.ordering.system.kafka.producer.service.impl;
 
 import com.food.ordering.system.kafka.producer.service.KafkaProducer;
+import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.avro.specific.SpecificRecordBase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
-import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.util.concurrent.ListenableFutureCallback;
 
 import java.io.Serializable;
 import java.util.concurrent.CompletableFuture;
@@ -25,11 +24,32 @@ public class KafkaProducerImpl<K extends Serializable, V extends SpecificRecordB
     }
 
     @Override
-    public void send(String topicName, K key, V message, ListenableFutureCallback<SendResult<K, V>> callback) {
+    public void send(String topicName, K key, V message, CompletableFuture<SendResult<K, V>> futureCallback) {
         log.info("Sending message: {} to topic: {}", message, topicName);
 
         CompletableFuture<SendResult<K, V>> kafkaResultFuture = kafkaTemplate.send(topicName, key, message);
+        kafkaResultFuture.whenComplete((successResult, exception) -> {
+            if (exception == null) {
+                handleSuccess(key, message, successResult);
+            } else {
+                handleFailure(key, message, exception);
+            }
+        });
+    }
 
-//        kafkaResultFuture.a
+    private void handleSuccess(K key, V message, SendResult<K,V> successResult) {
+        log.info("Message sent successfully for key: {}, message: {}, produced to partition: {}", key, message, successResult.getRecordMetadata().partition());
+    }
+
+    private void handleFailure(K key, V message, Throwable exception) {
+        log.error("Error on kafka producer while sending the message with key: {}, message: {} and exception: {}", key, message, exception.getMessage());
+    }
+
+    @PreDestroy
+    public void close() {
+        if (kafkaTemplate != null) {
+            log.info("Closing kafka producer!");
+            kafkaTemplate.destroy();
+        }
     }
 }
