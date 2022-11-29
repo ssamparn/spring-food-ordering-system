907e6b7791f12a48759ce80c58f5f51a17859d87
samantrs <https://samantrs@git.rabobank.nl>
2022-11-29 22:39:25 +0100

    migrated to spring boot 3.0.0

---
+++ order-service-infrastructure/order-service-kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/service/impl/KafkaProducerImpl.java
@@ -0,0 +1,35 @@
+package com.food.ordering.system.kafka.producer.service.impl;
+
+import com.food.ordering.system.kafka.producer.service.KafkaProducer;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import java.io.Serializable;
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+@Service
+public class KafkaProducerImpl<K extends Serializable, V extends SpecificRecordBase> implements KafkaProducer<K, V> {
+
+    private final KafkaTemplate<K, V> kafkaTemplate;
+
+    @Autowired
+    public KafkaProducerImpl(KafkaTemplate<K, V> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @Override
+    public void send(String topicName, K key, V message, ListenableFutureCallback<SendResult<K, V>> callback) {
+        log.info("Sending message: {} to topic: {}", message, topicName);
+
+        CompletableFuture<SendResult<K, V>> kafkaResultFuture = kafkaTemplate.send(topicName, key, message);
+
+//        kafkaResultFuture.a
+    }
+}
