1377f0c5d31c40e21fa86c05903257de156e8ae6
samantrs <https://samantrs@git.rabobank.nl>
2022-12-05 22:12:53 +0100

    payment service implementation completed

---
+++ payment-service/payment-container/src/main/resources/init-data.sql
@@ -0,0 +1,19 @@
+INSERT INTO payment.credit_entry(id, customer_id, total_credit_amount)
+VALUES ('8ee61791-100a-4974-b6f1-64f98644b513', '8ee61791-100a-4974-b6f1-64f98644b513', 500.00);
+
+INSERT INTO payment.credit_history(id, customer_id, amount, type)
+VALUES ('8ee61791-100a-4974-b6f1-64f98644b513', '8ee61791-100a-4974-b6f1-64f98644b513', 100.00, 'CREDIT');
+
+INSERT INTO payment.credit_history(id, customer_id, amount, type)
+VALUES ('8ee61791-100a-4974-b6f1-64f98644b513', '8ee61791-100a-4974-b6f1-64f98644b513', 600.00, 'CREDIT');
+
+INSERT INTO payment.credit_history(id, customer_id, amount, type)
+VALUES ('8ee61791-100a-4974-b6f1-64f98644b513', '8ee61791-100a-4974-b6f1-64f98644b513', 200.00, 'DEBIT');
+
+
+
+INSERT INTO payment.credit_entry(id, customer_id, total_credit_amount)
+VALUES ('8ee61791-100a-4974-b6f1-64f98644b513', '8ee61791-100a-4974-b6f1-64f98644b513', 100.00);
+
+INSERT INTO payment.credit_history(id, customer_id, amount, type)
+VALUES ('8ee61791-100a-4974-b6f1-64f98644b513', '8ee61791-100a-4974-b6f1-64f98644b513', 100.00, 'CREDIT');
